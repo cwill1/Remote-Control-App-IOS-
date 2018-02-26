8c01b16ee7a81d2632e6d5d7d7c92e557ac27029
CWILL <cwill@cwills-mbp>
2018-02-25 21:23:30 -0600

    Initial Commit

---
+++ Williams_C_MockRemoteControl3/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Williams_C_MockRemoteControl3
+//
+//  Created by CWILL on 2/25/18.
+//  Copyright © 2018 DePaul. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
